200e8b4abe0e7577bfff58413ecee2b885e48274
Pavel Germanovich <Germanovich>
2022-03-28 19:37:11 +0300

    first employee implementation

--- src/Demo.java
+++ src/Demo.java
@@ -6,10 +6,11 @@ public class Demo {
 
     private static void print() {
         System.out.println("");
+        print(3);
     }
 
     private static void print(int i) {
-
+        System.out.println("First employee " + i);
     }
 
     private static void print(String text) {
